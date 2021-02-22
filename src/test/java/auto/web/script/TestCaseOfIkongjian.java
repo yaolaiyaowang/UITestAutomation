e4a7f07c0c5d74b7ee994726f6dbc9398097fa6c
yaolaiyaowang <279054668@qq.com>
2021-02-22 13:51:18 +0800

    更改case用于修改缓存问题

--- src/test/java/auto/web/script/TestCaseOfIkongjian.java
+++ src/test/java/auto/web/script/TestCaseOfIkongjian.java
@@ -188,5 +188,132 @@ public class TestCaseOfIkongjian extends WebTestNGBase{
 			throw new FrameworkException(e.getMessage());
 		}
 	}
+	
+	
+	@Test(alwaysRun=false, groups={"auto.demo.test.ikongjian.groupTwo"}, priority=1,timeOut=MAX_EXCUTE_TIME)
+	public void caseOfWebGroupTwo()throws FrameworkException{
+		try{
+			InputStream inputStream = this.getClass().getClassLoader().getResourceAsStream("OrderUsefulInfo.properties");
+		    Properties properties = new Properties();
+			IKongJianWebPage ikongjianpage = new IKongJianWebPage(driver);
+			caseName = "生成预约单";
+			whichCaseIsRun(caseName);
+			properties.load(inputStream);
+			String myOrderListSize = properties.getProperty("myOrderListSize");
+			int myOrderListSizeInt = Integer.parseInt(myOrderListSize);
+			String testEnvironment = properties.getProperty("Environment");
+			
+			if(testEnvironment.equalsIgnoreCase("stage")){
+				ikongjianpage.loginIkongjian("wangliang1","Space521");
+			}else if(testEnvironment.equalsIgnoreCase("test7")){
+				ikongjianpage.loginIkongjian("superman","space521");
+			}
+			
+			for(int inum=0;inum<myOrderListSizeInt;inum++){
+				ikongjianpage.createPreOrder();
+			}
+			
+			osorderList.clear();
+			
+			ikongjianpage.openOrderPage();
+			
+			for(int i=0;i<phoneNumberList.size();i++){
+				ikongjianpage.changeOrderMessage(i);
+			}
+			
+			
+		}catch (Throwable e) {
+			e.printStackTrace();
+			throw new FrameworkException(e.getMessage());
+		}
+	}
+	
+	@Test(alwaysRun=false, groups={"auto.demo.test.ikongjian.groupThree"}, priority=1,timeOut=MAX_EXCUTE_TIME)
+	public void caseOfWebGroupThree()throws FrameworkException{
+		try{
+			InputStream inputStream = this.getClass().getClassLoader().getResourceAsStream("OrderUsefulInfo.properties");
+		    Properties properties = new Properties();
+			IKongJianWebPage ikongjianpage = new IKongJianWebPage(driver);
+			caseName = "生成预约单";
+			whichCaseIsRun(caseName);
+			properties.load(inputStream);
+			String myOrderListSize = properties.getProperty("myOrderListSize");
+			int myOrderListSizeInt = Integer.parseInt(myOrderListSize);
+			String testEnvironment = properties.getProperty("Environment");
+			
+			if(testEnvironment.equalsIgnoreCase("stage")){
+				ikongjianpage.loginIkongjian("wangliang1","Space521");
+			}else if(testEnvironment.equalsIgnoreCase("test7")){
+				ikongjianpage.loginIkongjian("superman","space521");
+			}
+			
+			for(int inum=0;inum<myOrderListSizeInt;inum++){
+				ikongjianpage.createPreOrder();
+			}
+			
+			osorderList.clear();
+			
+			ikongjianpage.openOrderPage();
+			
+			for(int i=0;i<phoneNumberList.size();i++){
+				ikongjianpage.changeOrderMessage(i);
+			}
+			
+			for(int j=0;j<phoneNumberList.size();j++){
+				ikongjianpage.payPreMoney(j);
+			}
+			
+			
+		}catch (Throwable e) {
+			e.printStackTrace();
+			throw new FrameworkException(e.getMessage());
+		}
+	}
+	
+	
+	@Test(alwaysRun=false, groups={"auto.demo.test.ikongjian.groupFour"}, priority=1,timeOut=MAX_EXCUTE_TIME)
+	public void caseOfWebGroupFour()throws FrameworkException{
+		try{
+			InputStream inputStream = this.getClass().getClassLoader().getResourceAsStream("OrderUsefulInfo.properties");
+		    Properties properties = new Properties();
+			IKongJianWebPage ikongjianpage = new IKongJianWebPage(driver);
+			caseName = "生成预约单";
+			whichCaseIsRun(caseName);
+			properties.load(inputStream);
+			String myOrderListSize = properties.getProperty("myOrderListSize");
+			int myOrderListSizeInt = Integer.parseInt(myOrderListSize);
+			String testEnvironment = properties.getProperty("Environment");
+			
+			if(testEnvironment.equalsIgnoreCase("stage")){
+				ikongjianpage.loginIkongjian("wangliang1","Space521");
+			}else if(testEnvironment.equalsIgnoreCase("test7")){
+				ikongjianpage.loginIkongjian("superman","space521");
+			}
+			
+			for(int inum=0;inum<myOrderListSizeInt;inum++){
+				ikongjianpage.createPreOrder();
+			}
+			
+			osorderList.clear();
+			
+			ikongjianpage.openOrderPage();
+			
+			for(int i=0;i<phoneNumberList.size();i++){
+				ikongjianpage.changeOrderMessage(i);
+			}
+			
+			for(int j=0;j<phoneNumberList.size();j++){
+				ikongjianpage.payPreMoney(j);
+			}
+			
+			for(int k=0;k<phoneNumberList.size();k++){
+				ikongjianpage.moveTheOrder(k);
+			}
+			
+		}catch (Throwable e) {
+			e.printStackTrace();
+			throw new FrameworkException(e.getMessage());
+		}
+	}
 
 }
